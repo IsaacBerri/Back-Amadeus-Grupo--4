b720f3251b8ac6345558ebf9ad6284dccd32f4e1
Isaac Berrio <berriisaax18@hotmail.com>
2024-10-23 18:03:31 -0500

    Cambiando la estructura de el proyecto

---
+++ src/main/java/com/example/nodo/demo/AmadeusBackApplication.java
@@ -0,0 +1,11 @@
+package com.example.nodo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/nodo/demo/AmadeusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nodo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
