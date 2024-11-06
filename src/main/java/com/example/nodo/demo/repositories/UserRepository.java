9e65fc2066eff4fcf5239ef650bf0e60037b9570
LuisaRami2018 <luisa.ramirezrpo10@gmail.com>
2024-11-06 08:16:24 -0500

    :sparkles: Add UserEntity model, repository, service and controller

---
+++ src/main/java/com/example/nodo/demo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.nodo.demo.repositories;
+
+import com.example.nodo.demo.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
