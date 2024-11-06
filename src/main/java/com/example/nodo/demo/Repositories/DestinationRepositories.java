a69a1a9e6ae77d9e0e27d7f878bab8ff1b54f70e
Jhon Arias <arias887@hotmail.com>
2024-11-06 08:35:24 -0500

    :sparkles: Add Destination Model and Repository

---
+++ src/main/java/com/example/nodo/demo/Repositories/DestinationRepositories.java
@@ -0,0 +1,8 @@
+package com.example.nodo.demo.Repositories;
+
+import com.example.nodo.demo.Models.Destination;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DestinationRepositories extends JpaRepository<Destination, Integer> {
+
+}
