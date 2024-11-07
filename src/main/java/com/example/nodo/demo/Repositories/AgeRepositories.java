5fd835ccb138f2577f59ed6f459524d610afe4f4
Jhon Arias <arias887@hotmail.com>
2024-11-06 22:32:57 -0500

    :sparkles: Add new Weather Files

--- src/main/java/com/example/nodo/demo/Repositories/AgeRepositories.java
+++
@@ -1,7 +0,0 @@
-package com.example.nodo.demo.Age.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgeRepositories extends JpaRepository<AgeRepositories, Integer> {
-
-}
