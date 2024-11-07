f71feca04f412314c90d4e94764a026ed1db1cfa
Jhon Arias <arias887@hotmail.com>
2024-11-06 22:27:45 -0500

    :sparkles: Add new Activity files

--- src/main/java/com/example/nodo/demo/Repositories/DestinationRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.example.nodo.demo.Repositories;
-
-import com.example.nodo.demo.Models.Destination;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinationRepositories extends JpaRepository<Destination, Integer> {
-
-}
--- src/main/java/com/example/nodo/demo/Repositories/TravelRepositorie.java
+++
@@ -1,7 +0,0 @@
-package com.example.nodo.demo.Travel.Repositories;
-
-import com.example.nodo.demo.Age.Repositories.AgeRepositories;import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelRepositorie extends JpaRepository<AgeRepositories.TravelRepositorie, Integer> {
-
-}
